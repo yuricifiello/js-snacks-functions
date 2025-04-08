f4208c7df17299f1bfa15b27e23905979786ba9f
yuricifiello <yuricifiello@gmail.com>
2025-04-08 15:43:54 +0200

    complete

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,25 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+function salutation(name) {
+  const currentHuors = new Date().getHours();
+  let salutation;
 
+  if (currentHuors < 13) {
+    salutation = "buongiorno";
+  } else if (currentHuors < 17) {
+    salutation = "buon pomeriggio";
+  } else {
+    salutation = "buonasera";
+  }
+
+  return `${salutation}, ${name}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(salutation("Mario"));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
