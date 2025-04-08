2c01009f471cdc9a5594d36c998f64e9cc7338d6
yuricifiello <yuricifiello@gmail.com>
2025-04-08 14:19:16 +0200

    first commit

---
+++ 05_bonus/script.js
@@ -0,0 +1,18 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+Il saluto deve essere :
+buongiorno se è mattina (fino alle 13), 
+buon pomeriggio se è pomeriggio (fino alle 17) 
+e buonasera se è sera (oltre le 17)
+*/
+
+const name = 'Mario';
+
+
+// Dichiara la funzione qui.
+
+
+// Invoca la funzione qui e stampa il risultato in console
+
+
+
+//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
